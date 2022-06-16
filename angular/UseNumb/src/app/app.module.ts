468491cf06d8e3b77bb236392b53945f41b94387
Thiago Rissati <thyago0730@gmail.com>
2022-06-15 21:54:15 -0300

    adicionando componentes

--- angular/UseNumb/src/app/app.module.ts
+++ angular/UseNumb/src/app/app.module.ts
@@ -3,14 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './views/header/header.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import { ButtonComponent } from './views/button/button.component';
+import {MatIconModule} from '@angular/material/icon';
+import { IconComponent } from './views/icon/icon.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ButtonComponent,
+    MatIconModule,
+    IconComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
